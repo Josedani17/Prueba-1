152cb0f9974569fd512f214fe4c21ec029609a0b
Josedani17 <105661559+Josedani17@users.noreply.github.com>
2023-03-03 20:37:26 +0100

    Add files via upload

---
+++ Jose_DanielGIT.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.jose_daniel.git;
+
+/**
+ *
+ * @author mecad
+ */
+public class Jose_DanielGIT {
+
+    public static void main(String[] args) {
+        System.out.println("Tarea ED04-2");
+        String nombre = "Jose";
+        String apellido = "Lozano";
+        String apellido2 = "Saura";
+        int edad = 25;
+        System.out.println(nombre + " " + apellido + " " + apellido2 + " " + edad );
+    }
+}
